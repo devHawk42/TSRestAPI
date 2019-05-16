a380cc646772828d21455581d780257c4da0153a
devHawk42 <devhawk42@gmail.com>
2019-05-16 15:11:40 -0300

    connect MySQL

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import express , { Application } from 'express';
+import morgan from "morgan";
 
 export class App {
     
     private app: Application;
 
-    constructor(){
+    constructor(private port?: number | string){
         this.app = express();
+        this.settings();
+        this.middlewares();
+    };
+
+    settings() {
+        this.app.set('port', this.port || process.env.PORT || 3000);
+    };
+
+    middlewares() {
+        this.app.use(morgan('dev'));
     }
 
     async listen(){
-        await this.app.listen(3000)
-        console.log('server on port', 3000)
-    }
+        await this.app.listen(this.app.get('port'))
+        console.log('server on port', this.app.get('port'))
+    };
 }
